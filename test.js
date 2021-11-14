09d89b6029ed6bc52e3956b583391d4688f4b62b
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-11-14 11:47:27 +0800

    broken

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ const myapp = require(".");
 describe('test1', () => {
     it('Should Return ...!', () => {
         myapp.isbn_to_json("0002-5232",(ok) => {
+            console.log(ok);
             expect(ok.book_name).to.equal("Algebra and Logic");
         });
     });
