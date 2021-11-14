74ebf8491671b65175aec8bde1c21cc333d3ff02
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-11-14 11:36:47 +0800

    Code optimization

--- index.js
+++ index.js
@@ -103,3 +103,73 @@ const chinese_book_name = '書刊名';
             callback(ok);
         });
 };
+
+exports.isbn_to_json = async function (isbn, callback) {
+    fetch(
+        `https://weblis.lib.ncku.edu.tw/search*cht/i?SEARCH=${isbn}&searchscope=1`
+    )
+        .then(response => response.text())
+        .then(text => {
+            var obj_book_info = {
+                book_isbn: isbn,
+                book_name: "",
+                book_info_s: []
+            };
+            var book_info_tepl = {
+                data_rec: "",
+                place_hold: "",
+                stor_loc: "",
+                stor_s: ""
+            };
+            var root = HTMLParser.parse(text);
+            var bibDetail = root.querySelectorAll(".bibDetail");
+            var bibHoldings = root.querySelector(".bibHoldings");
+            var BHR = bibHoldings.getElementsByTagName("tr");
+
+            var first_dont_count_holdingsDivider = 0;
+            for (var k = 0; k < BHR.length; k++) {
+
+                ele = BHR[k];
+                console.log('@' + trimOffNewlines(ele.getElementsByTagName("td")[0].innerText) + "@");
+
+                var data_mark = trimOffNewlines(ele.getElementsByTagName("td")[0].innerText);
+                if (data_mark === "資料識別" || ncrd.decode(data_mark) === "資料識別") {
+                    book_info_tepl.data_rec = ele.getElementsByTagName("td")[1].innerText;
+                }
+                if (data_mark === "架位號" || ncrd.decode(data_mark) === "架位號") {
+                    book_info_tepl.place_hold = ele.getElementsByTagName("td")[1].innerText;
+                }
+                if (data_mark === "館藏地" || ncrd.decode(data_mark) === "館藏地") {
+                    book_info_tepl.stor_loc = ele.getElementsByTagName("td")[1].innerText;
+                }
+                if (data_mark === "館藏" || ncrd.decode(data_mark) === "館藏") {
+                    book_info_tepl.stor_s = ele.getElementsByTagName("td")[1].innerText;
+                }
+                if (ele.innerHTML.includes("holdingsDivider")) {
+                    console.log("------------------------------------------------------holdingsDivider------------------------------------------------------------");
+                    if (first_dont_count_holdingsDivider !== 0) {
+                        obj_book_info.book_info_s.push(book_info_tepl);
+                        book_info_tepl = {
+                            data_rec: "",
+                            place_hold: "",
+                            stor_loc: "",
+                            stor_s: ""
+                        };
+                    }
+                    first_dont_count_holdingsDivider++;
+
+                }
+            }
+            bibDetail.forEach(element => {
+                if (element.text.includes(chinese_book_name)) {
+                    var book_name_ans = element.querySelector(".bibInfoData").text;
+                    obj_book_info.book_name = book_name_ans;
+                }
+            });
+            console.log(obj_book_info);
+            return obj_book_info;
+        })
+        .then(ok => {
+            callback(ok);
+        });
+};
--- test.js
+++ test.js
@@ -5,8 +5,8 @@ const myapp = require(".");
 
 describe('test1', () => {
     it('Should Return ...!', () => {
-        myapp.test_lib_qry("0002-5232",(ok) => {
-            expect(ok).to.equal("Algebra and Logic");
+        myapp.isbn_to_json("0002-5232",(ok) => {
+            expect(ok.book_name).to.equal("Algebra and Logic");
         });
     });
 });
