74717031f0c3071300dd5fdf3fa4e41a53503287
andythebreaker <e24074724@mail.ncku.edu.tw>
2022-02-27 00:47:21 +0800

    quick patch

--- index.js
+++ index.js
@@ -26,47 +26,49 @@ exports.isbn_to_json = async function (isbn, callback) {
             var root = HTMLParser.parse(text);
             var bibDetail = root.querySelectorAll(".bibDetail");
             var bibHoldings = root.querySelector(".bibHoldings");
-            var BHR = bibHoldings.getElementsByTagName("tr");
+            if (bibDetail && bibHoldings) {//TODO https://weblis.lib.ncku.edu.tw/search*cht/i?SEARCH=1027-5487&searchscope=1
+                var BHR = bibHoldings.getElementsByTagName("tr");
 
-            var first_dont_count_holdingsDivider = 0;
-            for (var k = 0; k < BHR.length; k++) {
-                ele = BHR[k];
+                var first_dont_count_holdingsDivider = 0;
+                for (var k = 0; k < BHR.length; k++) {
+                    ele = BHR[k];
 
-                var data_mark = trimOffNewlines(ele.getElementsByTagName("td")[0].innerText);
+                    var data_mark = trimOffNewlines(ele.getElementsByTagName("td")[0].innerText);
 
-                if (data_mark === "資料識別" || ncrd.decode(data_mark) === "資料識別") {
-                    book_info_tepl.data_rec = trimOffNewlines(ele.getElementsByTagName("td")[1].innerText);
-                }
-                if (data_mark === "架位號" || ncrd.decode(data_mark) === "架位號") {
-                    book_info_tepl.place_hold = trimOffNewlines(ele.getElementsByTagName("td")[1].innerText);
-                }
-                if (data_mark === "館藏地" || ncrd.decode(data_mark) === "館藏地") {
-                    book_info_tepl.stor_loc = trimOffNewlines(ele.getElementsByTagName("td")[1].innerText);
-                }
-                if (data_mark === "館藏" || ncrd.decode(data_mark) === "館藏") {
-                    book_info_tepl.stor_s = trimOffNewlines(ele.getElementsByTagName("td")[1].innerText);
-                }
-                if (ele.innerHTML.includes("holdingsDivider")) {
-                    if (first_dont_count_holdingsDivider !== 0) {
-                        obj_book_info.book_info_s.push(book_info_tepl);
-                        book_info_tepl = {
-                            data_rec: "",
-                            place_hold: "",
-                            stor_loc: "",
-                            stor_s: ""
-                        };
+                    if (data_mark === "資料識別" || ncrd.decode(data_mark) === "資料識別") {
+                        book_info_tepl.data_rec = trimOffNewlines(ele.getElementsByTagName("td")[1].innerText);
                     }
-                    first_dont_count_holdingsDivider++;
+                    if (data_mark === "架位號" || ncrd.decode(data_mark) === "架位號") {
+                        book_info_tepl.place_hold = trimOffNewlines(ele.getElementsByTagName("td")[1].innerText);
+                    }
+                    if (data_mark === "館藏地" || ncrd.decode(data_mark) === "館藏地") {
+                        book_info_tepl.stor_loc = trimOffNewlines(ele.getElementsByTagName("td")[1].innerText);
+                    }
+                    if (data_mark === "館藏" || ncrd.decode(data_mark) === "館藏") {
+                        book_info_tepl.stor_s = trimOffNewlines(ele.getElementsByTagName("td")[1].innerText);
+                    }
+                    if (ele.innerHTML.includes("holdingsDivider")) {
+                        if (first_dont_count_holdingsDivider !== 0) {
+                            obj_book_info.book_info_s.push(book_info_tepl);
+                            book_info_tepl = {
+                                data_rec: "",
+                                place_hold: "",
+                                stor_loc: "",
+                                stor_s: ""
+                            };
+                        }
+                        first_dont_count_holdingsDivider++;
+                    }
                 }
-            }
-            bibDetail.forEach(element => {
-                if (element.text.includes(chinese_book_name)) {
-                    var book_name_ans = element.querySelector(".bibInfoData").text;
-                    obj_book_info.book_name = trimOffNewlines(book_name_ans);
-                }
-            });
-            console.log(obj_book_info);
-            return obj_book_info;
+                bibDetail.forEach(element => {
+                    if (element.text.includes(chinese_book_name)) {
+                        var book_name_ans = element.querySelector(".bibInfoData").text;
+                        obj_book_info.book_name = trimOffNewlines(book_name_ans);
+                    }
+                });
+                console.log(obj_book_info);
+                return obj_book_info;
+            } else { return null; }
         })
         .then(ok => {
             callback(ok);
